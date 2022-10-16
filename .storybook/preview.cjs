aff418c47fd08774cd2d8136f5ad44d644bb167e
lucasrasec <lucasrasecc@gmail.com>
2022-10-16 10:33:12 -0300

    adding tests in storybook using jest and msw

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,14 +1,22 @@
 import { themes } from "@storybook/theming";
+import "../src/styles/global.css"
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize({
+    onUnhandkedRequest: "bypass"
+})
+
+export const decorators = [mswDecorator]
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-  docs: {
-    theme: themes.dark,
-  },
+    docs: {
+        theme: themes.dark,
+    },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -5,74 +5,9 @@ import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
 import { Text } from "./components/Text";
 import { Logo } from "./Logo";
+import { SignIn } from "./pages/SignIn";
 import "./styles/global.css";
 
 export function App() {
-  return (
-    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
-      <header className="flex flex-col items-center">
-        <Logo />
-
-        <Heading size="lg" className="mt-4">
-          Ignite Lab
-        </Heading>
-
-        <Text size="lg" className="text-gray-400 mt-1">
-          Faça login e começe a usar
-        </Text>
-      </header>
-
-      <form className="flex flex-col gap-4 items-sctretch w-full max-w-sm mt-10">
-        <label htmlFor="email" className="flex flex-col gap-3">
-          <Text className="font-bold">Endereço de e-mail</Text>
-          <Input.Root>
-            <Input.Icon>
-              <Envelope />
-            </Input.Icon>
-            <Input.Input
-              type="email"
-              id="email"
-              placeholder="Digite seu E-mail"
-            />
-          </Input.Root>
-        </label>
-
-        <label htmlFor="password" className="flex flex-col gap-3">
-          <Text className="font-bold">Endereço de e-mail</Text>
-          <Input.Root>
-            <Input.Icon>
-              <Lock />
-            </Input.Icon>
-            <Input.Input
-              type="password"
-              id="password"
-              placeholder="***********"
-            />
-          </Input.Root>
-        </label>
-
-        <label htmlFor="remenber" className="flex items-center gap-2">
-          <CheckBox id="remenber" />
-          <Text size="sm" className="text-gray-200">
-            Lembrar de mim por 30 dias
-          </Text>
-        </label>
-
-        <Button type="submit">Entrar na plataforma</Button>
-      </form>
-
-      <footer className="flex flex-col items-center gap-4 mt-8">
-        <Text asChild size="sm">
-          <a href="" className="text-gray-400 underline hover:text-gray-200">
-            Esqueceu sua senha?
-          </a>
-        </Text>
-        <Text asChild size="sm">
-          <a href="" className="text-gray-400 underline hover:text-gray-200">
-            Não possui conta? Crie uma agora!
-          </a>
-        </Text>
-      </footer>
-    </div>
-  );
+  return (<SignIn/>)
 }
